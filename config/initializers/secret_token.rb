3b2242fac8274dd50aa42681fa0138d1c338849c
Esdras Eduardo <esdrasedu@gmail.com>
2013-04-25 13:45:13 +0000

    Init App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AngularRestfulServer::Application.config.secret_token = 'c6b0e3fb74715005da30637abde1d0cc8f42eff9cc62befa2981de04db2577192313e5e559697fc88c889c08f7fdaad8c71a3ec0dca4b5b6b3d758a9d0d45250'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+AngularRestfulServer::Application.config.session_store :cookie_store, key: '_angular-restful-server_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# AngularRestfulServer::Application.config.session_store :active_record_store
